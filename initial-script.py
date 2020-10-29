2b8d548c1f9526cdaafcd984d4888491dc94ab1c
David Li <xxinli.1983@gmail.com>
2020-10-29 23:59:16 +1100

    demo

--- initial-script.py
+++ initial-script.py
@@ -1,1 +1,8 @@
 print('THis is wrong!')
+name = input()
+print('Name: ' + name)
+
+first = int(input('first number:'))
+second = int(input('second number:'))
+sum = first + second
+print('sum :' + str(sum))
